75fa546562f97cac7ec9970b7f22707547170548
Sijun <sijun_niu@brown.edu>
2017-10-06 20:15:53 -0400

    UEL_2D

--- EN234_FEA/user_codes/src/abaqus_uel_2D.for
+++ EN234_FEA/user_codes/src/abaqus_uel_2D.for
@@ -11,7 +11,7 @@
 !          abq_UEL_1D_integrationpoints(n_points, n_nodes, xi, w)  = defines integration points for 1D line integral
 !=========================== ABAQUS format user element subroutine ===================
 
-      SUBROUTINE UEL_2D(RHS,AMATRX,SVARS,ENERGY,NDOFEL,NRHS,NSVARS,
+      SUBROUTINE UEL(RHS,AMATRX,SVARS,ENERGY,NDOFEL,NRHS,NSVARS,
      1     PROPS,NPROPS,COORDS,MCRD,NNODE,U,DU,V,A,JTYPE,TIME,DTIME,
      2     KSTEP,KINC,JELEM,PARAMS,NDLOAD,JDLTYP,ADLMAG,PREDEF,NPREDF,
      3     LFLAGS,MLVARX,DDLMAG,MDLOAD,PNEWDT,JPROPS,NJPROP,PERIOD)
@@ -142,8 +142,70 @@
       if (NNODE == 9) n_points = 9             ! Quadratic rect
 
     ! Write your code for a 2D element below
+      
+      call abq_UEL_2D_integrationpoints(n_points, NNODE, xi, w)
+      
+      if (MLVARX<2*NNODE) then
+        write(6,*) ' Error in abaqus UEL '
+        write(6,*) ' Variable MLVARX must exceed 2*NNODE'
+        write(6,*) ' MLVARX = ',MLVARX,' NNODE = ',NNODE
+        stop
+      endif
+      
+      RHS(1:MLVARX,1) = 0.d0
+      AMATRX(1:NDOFEL,1:NDOFEL) = 0.d0
+      
+      D = 0.d0
+      E = PROPS(1)
+      xnu = PROPS(2)
+      d44 = 0.5D0*E/(1+xnu)
+      d11 = (1.D0-xnu)*E/( (1+xnu)*(1-2.D0*xnu) )
+      d12 = xnu*E/( (1+xnu)*(1-2.D0*xnu) )
+      D(1:3,1:3) = d12
+      D(1,1) = d11
+      D(2,2) = d11
+      D(3,3) = d11
+      D(4,4) = d44
+      
+      ENERGY(1:8) = 0.d0
 
-      END SUBROUTINE UEL_2D
+    !     --  Loop over integration points
+      do kint = 1, n_points
+          call abq_UEL_2D_shapefunctions(xi(1:2,kint),NNODE,N,dNdxi)
+          dxdxi = matmul(coords(1:2,1:NNODE),dNdxi(1:NNODE,1:2))
+          call abq_inverse_LU(dxdxi,dxidx,2,determinant)
+          dNdx(1:NNODE,1:2) = matmul(dNdxi(1:NNODE,1:2),dxidx)
+          B = 0.d0
+          B(1,1:2*NNODE-1:2) = dNdx(1:NNODE,1)
+          B(2,2:2*NNODE:2)   = dNdx(1:NNODE,2)
+          B(4,1:2*NNODE-1:2) = dNdx(1:NNODE,2)
+          B(4,2:2*NNODE:2)   = dNdx(1:NNODE,1)
+          
+          strain = matmul(B(1:4,1:2*NNODE),U(1:2*NNODE))
+
+          stress = matmul(D,strain)
+          RHS(1:2*NNODE,1) = RHS(1:2*NNODE,1)
+     1     - matmul(transpose(B(1:4,1:2*NNODE)),stress(1:4))*
+     2                                          w(kint)*determinant
+
+          AMATRX(1:2*NNODE,1:2*NNODE) = AMATRX(1:2*NNODE,1:2*NNODE)
+     1    + matmul(transpose(B(1:4,1:2*NNODE)),
+     2                   matmul(D,B(1:4,1:2*NNODE)))
+     3                                             *w(kint)*determinant
+
+          ENERGY(2) = ENERGY(2)
+     1     + 0.5D0*dot_product(stress,strain)*w(kint)*determinant           ! Store the elastic strain energy
+
+          if (NSVARS>=n_points*4) then   ! Store stress at each integration point (if space was allocated to do so)
+              SVARS(4*kint-3:4*kint) = stress(1:4)
+          endif
+      end do
+      
+      PNEWDT = 1.d0          ! This leaves the timestep unchanged (ABAQUS will use its own algorithm to determine DTIME)
+      
+      return
+      
+      END SUBROUTINE UEL
 
 
 
@@ -572,7 +634,7 @@
 
       end subroutine abq_facenodes_2d
 
-      subroutine abq_inverse_LU(Ain,A_inverse,n)  ! Compute the inverse of an arbitrary matrix by LU decomposition
+      subroutine abq_inverse_LU(Ain,A_inverse,n,determinant)  ! Compute the inverse of an arbitrary matrix by LU decomposition
 
         implicit none
 
@@ -580,6 +642,7 @@
 
         double precision, intent(in)    :: Ain(n,n)
         double precision, intent(out)   :: A_inverse(n,n)
+        double precision, intent(out)   :: determinant
 
         double precision :: A(n,n), L(n,n), U(n,n), b(n), d(n), x(n)
         double precision :: coeff
@@ -589,6 +652,9 @@
         L=0.d0
         U=0.d0
         b=0.d0
+        
+!    Here I just assumed n=2 and calculated the determinant, NOT a general case
+        determinant = A(n-1,n-1)*A(n,n)-A(n-1,n)*A(n,n-1)
 
         do k=1, n-1
             do i=k+1,n
